5ff134a1f422e5e9e1cc22e5ba1cea4e89c607c4
raulubillos-santex <104849358+raulubillos-santex@users.noreply.github.com>
2022-05-15 21:01:11 -0300

    Swagger (#5)
    * Added swagger routes
    
    * Updated open api version
    
    Co-authored-by: raul <raul.ubillos@sameskyhealth.com>

--- src/swagger/index.js
+++ src/swagger/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const Swagger = require('./swagger.json');
-const router = express.Router()
+const router = express.Router();
 
 router.use('/',swaggerUi.serve, swaggerUi.setup(Swagger));
 
