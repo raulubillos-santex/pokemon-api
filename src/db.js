70aa08843ee415a2190a6584fd76457d7ea6b375
raulubillos-santex <104849358+raulubillos-santex@users.noreply.github.com>
2022-05-17 20:49:15 -0300

    Database (#11)
    * Added folder for user
    
    * Added placeholder files
    
    * Renamed folder
    
    * Added db connection
    
    * Added file for db
    
    * Added database models
    
    * Added database models
    
    Co-authored-by: raul <raul.ubillos@sameskyhealth.com>

---
+++ src/db.js
@@ -0,0 +1,95 @@
+
+const {Sequelize, DataTypes} = require('sequelize');
+
+const sequelize = new Sequelize({
+    dialect: process.env.DBTYPE || 'mysql',
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
+    username: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database:process.env.DBSCHEMA,
+})
+
+const PokemonModel = sequelize.define('Pokemon', {
+    Id:{
+        primaryKey:true,
+        type: DataTypes.UUID
+    },
+    Name:{
+        type: DataTypes.TEXT
+    },
+    Type:{
+        type:DataTypes.TEXT
+    },
+    Level:{
+        type:DataTypes.INTEGER
+    },
+    Strength:{
+        type:DataTypes.INTEGER
+    },
+    Vitality:{
+        type:DataTypes.INTEGER
+    },
+    Speed:{
+        type:DataTypes.INTEGER
+    },
+    TeamId:{
+        type:DataTypes.UUID
+    },
+    TrainerId:{
+        type:DataTypes.UUID
+    }
+}, {tableName:"Pokemon"});
+
+const TeamModel = sequelize.define('Team', {
+    Id:{
+        primaryKey:true,
+        type: DataTypes.UUID
+    },
+    Name:{
+        type: DataTypes.TEXT
+    },
+    CreationDate:{
+        type: DataTypes.DATE
+    },
+    TrainerId:{
+        type: DataTypes.UUID
+    }
+}, {tableName:"Team"});
+
+TeamModel.hasMany(PokemonModel,{foreignKey:"TeamId"});
+
+const TrainerModel = sequelize.define('Trainer', {
+    Id:{
+        primaryKey:true,
+        type: DataTypes.UUID
+    },
+    TrainerId:{
+        type: DataTypes.TEXT
+    },
+    Name:{
+        type:DataTypes.TEXT
+    },
+    Surname:{
+        type:DataTypes.TEXT
+    },
+    Gender:{
+        type:DataTypes.CHAR
+    },
+    Age:{
+        type:DataTypes.INTEGER
+    },
+    Password:{
+        type:DataTypes.TEXT
+    },
+    Region:{
+        type:DataTypes.TEXT
+    }
+}, {tableName:"Trainer"});
+
+TrainerModel.hasMany(TeamModel,{foreignKey:"TrainerId"})
+TrainerModel.hasMany(PokemonModel,{foreignKey:"TrainerId"})
+
+
+
+module.exports=sequelize
