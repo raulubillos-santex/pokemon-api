ec01d65e723d89c93fa4fb99f4d91c64e68c5468
raul <raul.ubillos@sameskyhealth.com>
2022-05-17 20:48:10 -0300

    Added database models

--- src/server.js
+++ src/server.js
@@ -10,7 +10,6 @@ var http = require('http');
 var dotenv = require('dotenv');
 dotenv.config()
 var sequelize = require('./db');
-
 /**
 * Get port from environment and store in Express.
 */
@@ -85,10 +84,6 @@ function onError(error) {
 */
 
 async function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-  await sequelize.authenticate();
+  await sequelize.sync({alter:true,logging:false})
 }
+
