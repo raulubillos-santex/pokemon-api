17ec5ec5ee35a724d053c3ffb412efec18fe7fb1
Raziel <ivangaitan.kp@gmail.com>
2022-07-11 22:55:55 -0300

    express homework-1

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -13,6 +13,22 @@ router.get('/', isAuthenticated, validateIfNotValidatorError, getPokemonListForT
 
 router.post('/capture', isAuthenticated, ...validateCaptureBody, validatePokemonSpecie, validateIfNotValidatorError, capture);
 router.get('/:name', isAuthenticated, getPokemonByNameForTrainer);
+router.get('/:name/data', isAuthenticated, async(req, res, next) => {
+    const pokemon = await pokemonByTrainer(req.user.Id, req.params.name);
+    if (pokemon) {
+        res.status(200).send(
+            `El pokemon es ${req.params.name}`
+        );
+        return next();
+    }
+
+    res.status(204).send({
+        httpStatus: 204,
+        errorCode: 'POKEMON_NOT_FOUND'
+    });
+    return next();
+});
+
 
 router.delete('/:name/release', isAuthenticated, release);
 
