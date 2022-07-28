9fc70e1a2f6876c507cb24cc53b38f0f510bcdb2
Francisco Martinengo <frmartinengo@hotmail.com>
2022-07-28 10:36:02 -0300

    Ejercicio2 Express para Santex

---
+++ src/routes/file.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+const { isAuthenticated } = require('../middlewares/general');
+
+const { writefileDelete, writefilePut, writefilePost, writefileGet } = require('../controllers/file');
+
+module.exports = router;
+router.get('/writeFileGet', isAuthenticated, writefileGet);
+
+router.post('/writeFilePost', isAuthenticated, writefilePost);
+
+router.put('/:pokemonName/writeFilePut', isAuthenticated, writefilePut);
+
+router.delete('/writeFileDelete', isAuthenticated, writefileDelete);
+
+module.exports = router; 
